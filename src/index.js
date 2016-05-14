4296fb458f327338d94704e657b9118d6cae92a7
Jim Hooper <jim_hooper@volusion.com>
2016-05-14 13:33:43 -0500

    Scanner Obj

--- src/index.js
+++ src/index.js
@@ -1,47 +1,67 @@
-(function(){
+ var stockScanner = (function() {
+     var config = {
+         tkCredsJSON: "/json/tk-creds.json",
+         tkApiUrl :  "https://api.tradeking.com/v1/market/ext/quotes.json?symbols=aapl,fb,acc",
+         symbolsJSON : "/json/symbols.json",
+     }
 
-   
+     var quotesData = {};
 
-window.tkData = {};
-$.getJSON("/json/tk-creds.json", function(data) {
+     var formatSymbols = function(){
+        $.getJSON(config.symbolsJSON, function(data) {
+            
+            $.each(data.symbols, function(k, v){
 
-    var creds = data;
+                    window.console.log(v.symbol);
+            });
+            
 
-    var oauth = OAuth({
-        consumer: {
-            public: creds.consumer_key,
-            secret: creds.consumer_secret
-        },
-        signature_method: 'HMAC-SHA1'
-    });
-    var token = {
-        public: creds.access_token,
-        secret: creds.access_secret
-    };
-    var request_data = {
-        url: 'https://api.tradeking.com/v1/market/ext/quotes.json?symbols=aapl',
-        method: 'GET'
-    };
-    $.ajax({
-        url: request_data.url,
-        type: request_data.method,
-        data: oauth.authorize(request_data, token)
-    }).error(function(err) {
-      
-       window.console.log(err);
-    }).done(function(data) {
-      
-   
-        window.tkData = data;
-    });
-    
-});
+        });
+     }
+     // Call tradeking api
+     var callApi = function() {
+         $.getJSON(config.tkCredsJSON, function(data) {
 
-window.console.log(window.tkData);
+             var creds = data;
 
-})();
+             var oauth = OAuth({
+                 consumer: {
+                     public: creds.consumer_key,
+                     secret: creds.consumer_secret
+                 },
+                 signature_method: 'HMAC-SHA1'
+             });
+             var token = {
+                 public: creds.access_token,
+                 secret: creds.access_secret
+             };
+             var request_data = {
+                 url: config.tkApiUrl,
+                 method: 'GET'
+             };
+             $.ajax({
+                 url: request_data.url,
+                 type: request_data.method,
+                 data: oauth.authorize(request_data, token)
+             }).error(function(err) {
 
-// make transferrable to other trading sites
-// draw out flow chart and timing
-// create a stock simulator going up and down by one cent.
-// enter in my trade and create a switch statement that executes the opposite etc... buy cover
+                 window.console.log("Bad TK Request", err);
+             }).done(function(data) {
+
+                 quotesData = data;
+             });
+
+         });
+     }
+
+     return {
+         init: function() {
+                formatSymbols();
+             // callApi();
+
+         }
+     };
+
+ })();
+
+ stockScanner.init(); 
